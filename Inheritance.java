926d008c875308009e9ac99189a5f831bf1925ab
kiruba192k2 <jonashtyler@gmail.com>
2026-06-19 16:23:09 +0530

    UPDATE

--- Inheritance.java
+++ Inheritance.java
@@ -1,71 +1,71 @@
-class Employee {
+// class Employee {
 
-    int empId;
-    String name;
-    double salary;
+//     int empId;
+//     String name;
+//     double salary;
 
-    Employee(int empId, String name, double salary) {
-        this.empId = empId;
-        this.name = name;
-        this.salary = salary;
-    }
-}
-class Tester extends Employee{
-    String software;
+//     Employee(int empId, String name, double salary) {
+//         this.empId = empId;
+//         this.name = name;
+//         this.salary = salary;
+//     }
+// }
+// class Tester extends Employee{
+//     String software;
 
-    public Tester(String software, int empId, String name, double salary) {
-        super(empId, name, salary);
-        this.software = software;
-    }
-     void display() {
-        System.out.println("Employee ID : " + empId);
-        System.out.println("Employee Name : " + name);
-        System.out.println("Employee Salary : " + salary);
-        System.out.println("Software Known: " + software);
-    }
+//     public Tester(String software, int empId, String name, double salary) {
+//         super(empId, name, salary);
+//         this.software = software;
+//     }
+//      void display() {
+//         System.out.println("Employee ID : " + empId);
+//         System.out.println("Employee Name : " + name);
+//         System.out.println("Employee Salary : " + salary);
+//         System.out.println("Software Known: " + software);
+//     }
     
 
-}
+// }
 
-class Developer extends Employee {
+// class Developer extends Employee {
 
-    String programmingLanguage;
+//     String programmingLanguage;
 
-    Developer(int empId,
-              String name,
-              double salary,
-              String programmingLanguage) {
+//     Developer(int empId,
+//               String name,
+//               double salary,
+//               String programmingLanguage) {
 
-        super(empId, name, salary);
-        this.programmingLanguage = programmingLanguage;
-    }
+//         super(empId, name, salary);
+//         this.programmingLanguage = programmingLanguage;
+//     }
 
-    void display() {
-        System.out.println("Employee ID : " + empId);
-        System.out.println("Employee Name : " + name);
-        System.out.println("Employee Salary : " + salary);
-        System.out.println("Programming Language : " + programmingLanguage);
-    }
-}
+//     void display() {
+//         System.out.println("Employee ID : " + empId);
+//         System.out.println("Employee Name : " + name);
+//         System.out.println("Employee Salary : " + salary);
+//         System.out.println("Programming Language : " + programmingLanguage);
+//     }
+// }
 
-public class Inheritance {
+// public class Inheritance {
 
-    public static void main(String[] args) {
+//     public static void main(String[] args) {
 
-        Developer dev = new Developer(
-                101,
-                "Ram",
-                50000,
-                "Java"
-        );
+//         Developer dev = new Developer(
+//                 101,
+//                 "Ram",
+//                 50000,
+//                 "Java"
+//         );
 
-        dev.display();
-        Tester tes=new Tester("CTest",
-         103,
-          "yuvaraj",
-           35999);
-           tes.display();
+//         dev.display();
+//         Tester tes=new Tester("CTest",
+//          103,
+//           "yuvaraj",
+//            35999);
+//            tes.display();
 
         
-    }
-}
+//     }
+// }
--- MethodOverriding.java
+++ MethodOverriding.java
@@ -1,51 +1,51 @@
-public class MethodOverriding {
- 
-    public static void main(String[] args) {
- 
-        Emp1 e1 = new Emp1(10, "kiruba", 50000);
-        e1.display();
- 
-        System.out.println();
- 
-        Developer dev = new Developer(11, "kannan", 10000, "Java");
-        dev.display();
-    }
-}
- 
-class Emp1 {
- 
-    int id;
-    String name;
-    double salary;
- 
-    Emp1(int id, String name, double salary) {
-        this.id = id;
-        this.name = name;
-        this.salary = salary;
-    }
- 
-    void display() {
-        System.out.println("Employee ID      : " + id);
-        System.out.println("Employee Name    : " + name);
-        System.out.println("Employee Salary  : Rs. " + salary);
-    }
-}
- 
-class Developer extends Emp1 {
- 
-    String programmingLanguage;
- 
-    Developer(int id, String name, double salary, String programmingLanguage) {
-        super(id, name, salary);
-        this.programmingLanguage = programmingLanguage;
-    }
- 
-    @Override
-    void display() {
-        System.out.println("Employee ID      : " + id);
-        System.out.println("Employee Name    : " + name);
-        System.out.println("Employee Salary  : Rs. " + salary);
-        System.out.println("Programming Language : " + programmingLanguage);
-    }
-}
- 
+
+// public class MethodOverriding {
+
+//     public static void main(String[] args) {
+
+//         Emp1 e1 = new Emp1(10, "kiruba", 50000);
+//         e1.display();
+
+//         System.out.println();
+
+//         Developer dev = new Developer(11, "kannan", 10000, "Java");
+//         dev.display();
+//     }
+// }
+
+// class Emp1 {
+
+//     int id;
+//     String name;
+//     double salary;
+
+//     Emp1(int id, String name, double salary) {
+//         this.id = id;
+//         this.name = name;
+//         this.salary = salary;
+//     }
+
+//     void display() {
+//         System.out.println("Employee ID      : " + id);
+//         System.out.println("Employee Name    : " + name);
+//         System.out.println("Employee Salary  : Rs. " + salary);
+//     }
+// }
+
+// class Developer extends Emp1 {
+
+//     String programmingLanguage;
+
+//     Developer(int id, String name, double salary, String programmingLanguage) {
+//         super(id, name, salary);
+//         this.programmingLanguage = programmingLanguage;
+//     }
+
+//     @Override
+//     void display() {
+//         System.out.println("Employee ID      : " + id);
+//         System.out.println("Employee Name    : " + name);
+//         System.out.println("Employee Salary  : Rs. " + salary);
+//         System.out.println("Programming Language : " + programmingLanguage);
+//     }
+// }
